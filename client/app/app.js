4f993a12003f63c29cbf748e88292809b24f9462
Ivan Afanasov <i_van@list.ru>
2014-11-24 16:52:59 +0300

    fixed  &nbsp in header; smiles support have been added;

--- client/app/app.js
+++ client/app/app.js
@@ -7,7 +7,8 @@ angular.module('simpleChatApp', [
     'btford.socket-io',
     'ui.router',
     'ui.bootstrap',
-    'angularMoment'
+    'angularMoment',
+    'emoji'
 ])
     .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
         $urlRouterProvider
