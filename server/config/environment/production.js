e49dc47550d45c020e4d93e952530d50b2eb7a56
Ivan Afanasov <i_van@list.ru>
2015-01-28 18:48:40 +0300

    fixed config for production;

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -11,8 +11,6 @@ module.exports = {
 
     // MongoDB connection options
     mongo: {
-        uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL
-            || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME
-            || 'mongodb://localhost/simplechat'
+        uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/simplechat'
     }
 };
