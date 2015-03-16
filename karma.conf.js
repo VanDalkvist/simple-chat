8516503eb01d7ce35c9ce525466b626467e38554
Ivan Afanasov <i_van@list.ru>
2015-03-16 15:26:29 +0300

    stub to rebuild

--- karma.conf.js
+++ karma.conf.js
@@ -74,7 +74,9 @@ module.exports = function (config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['PhantomJS'],
+        browsers: [
+            'PhantomJS'
+        ],
 
 
         // Continuous Integration mode
